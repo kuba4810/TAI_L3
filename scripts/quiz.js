ea8d048aed4267d7c8af183a2a3abeacc59ec3e1
kuba4810 <kuba__koziol@op.pl>
2018-10-12 16:51:21 +0200

    Finished

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -60,6 +60,30 @@ var questions = [
          "correctAnswer": "Z Cimmerri"
     }
 ]
+
+function showScoreHistory(){
+    var history = JSON.parse(localStorage.getItem("resultHistory"));
+    console.log(history);
+    var i=0;
+
+    console.log("Działa funkcja showScoreHistory");
+    for(i;i<history.length;i++){
+        var hItem = history[i];
+        
+        var row = document.createElement("tr");
+
+        var data = document.createElement("td");
+        data.appendChild( document.createTextNode(hItem.date) );
+
+        var score = document.createElement("td");
+        score.appendChild( document.createTextNode(hItem.score) );
+
+        row.appendChild(data);
+        row.appendChild(score);
+
+        document.querySelector("#scoreTableBody").appendChild(row);
+    }
+}
 function showQuestion(q){
     document.getElementById("questionText").innerHTML = q.question;
 
@@ -69,49 +93,54 @@ function showQuestion(q){
     document.getElementById("d").innerHTML = q.answers[3];
 }
 
-localStorage.setItem("resulthistory","0");
+//localStorage.setItem("resultHistory", JSON.stringify([]));
+
+var resultHistory = JSON.parse(localStorage.getItem("resultHistory"));
+localStorage.setItem("resultHistory", JSON.stringify(resultHistory));
 localStorage.setItem("result","0");
 var index = 0;
+
 var progresBarWidth = index+1;
 var question = questions[index];
 showQuestion(question);
 
-var time = 20;
-    
-function myTimeout(){
-    return (setTimeout(function(){
-        index = index+1;
-        question = questions[index];
-        showQuestion(question);
-        clearInterval(myInterval);
-        time=20;
-        document.getElementById("time").classList.add("progress-bar-success");
-    },20000))
-}
+var time = 10;
 
-function myInterval(){
+
+function startInterval(){
     return(setInterval(function(){
         if(time > 0){
             time = time -1;
-            document.getElementById("time").style.width = (time/2)*10 +"%";
+            document.getElementById("time").style.width = (time)*10 +"%";
             document.getElementById("time").innerHTML =  time+"s";
         }
     
     
-        if(time <=10){
+        if(time <=6){
            document.getElementById("time").classList.remove("progress-bar-success");
            document.getElementById("time").classList.add("progress-bar-warning");
         }
-        if(time <=5){
+        if(time <=3){
            document.getElementById("time").classList.remove("progress-bar-success");
            document.getElementById("time").classList.add("progress-bar-danger");
+        }
+    
+        if(time<=0){
+            document.getElementById("time").classList.remove("progress-bar-danger");
+            document.getElementById("time").classList.remove("progress-bar-warning");
+            document.getElementById("time").classList.add("progress-bar-success");
+            index = index+1;
+            question = questions[index];
+            showQuestion(question);
+            time=11;
+            progresBarWidth = progresBarWidth+1;
+            document.getElementById("progress").innerHTML = progresBarWidth + "/10";
+            document.getElementById("progress").style.width = progresBarWidth + '0%';
         }
     },1000))
 }
 
-
-myTimeout();
-myInterval();
+var myInterval = startInterval();
 
 
 
@@ -120,6 +149,7 @@ var i=0;
 for(i;answers.length ; i++){
     answers[i].addEventListener('click',function(e){
         
+        clearInterval(myInterval);
          var userAnswer = e.target.textContent;
 
          if(userAnswer == question.correctAnswer ){
@@ -137,25 +167,51 @@ for(i;answers.length ; i++){
          }
 
          if(index < 9){
-            
-            setTimeout(function(){
-                myTimeout();
-                myInterval();
-                index = index+1;
-                progresBarWidth = progresBarWidth+1;
+                clearInterval(myInterval);
+                time=11;
+                progresBarWidth = progresBarWidth+1;               
+
+                document.getElementById("time").classList.remove("progress-bar-danger");
+                document.getElementById("time").classList.remove("progress-bar-warning");
+                document.getElementById("time").classList.add("progress-bar-success");
+                myInterval = startInterval();          
+
+
+             setTimeout(function(){
+                index = index+1;               
                 question = questions[index];
                 showQuestion(question);
                 document.getElementById("progress").style.width = progresBarWidth+"0%";
                 document.getElementById("progress").innerHTML = progresBarWidth + "/10";
-                e.target.removeAttribute("style");
-             },500);
+                e.target.removeAttribute("style");     
+              
+             },1000);
+           
          }
          else{
+            time=11;
+            
             setTimeout(function(){
                 var summary = localStorage.getItem("result");
-                document.getElementById("result").innerHTML = "Udało Ci się ukończyć Quiz ! Twój wynik to : " + summary;
+                document.querySelector(".score").innerHTML = "Udało Ci się ukończyć Quiz ! Twój wynik to : " + summary;
                 e.target.removeAttribute("style");
 
+                resultHistory = JSON.parse(localStorage.getItem("resultHistory"));
+
+                var date = new Date();
+
+                resultHistory.unshift({ date: date.getDate()+"."+date.getMonth()+"."+date.getFullYear()+"/ "+
+                 date.getHours()+":"+date.getMinutes()+":"+(date.getSeconds()> 10 ? date.getSeconds() : "0"+date.getSeconds()),score:summary});
+
+                localStorage.setItem("resultHistory",JSON.stringify(resultHistory));
+                console.log(localStorage.getItem("resultHistory"));
+
+                //document.querySelector(".questionText").classList.add("invisible");
+               // document.querySelector(".answers").classList.add("invisible");
+
+               showScoreHistory();
+
+               document.getElementById("scoreHistory").classList.remove("invisible");
              
              },500);
          
